4356a797e81ba8f99ab2b779be69afc06445fc8b
Lakhwinder <lakhwinder_gill@hotmail.com>
2020-05-28 21:23:03 +1000

    Created solution

--- HandsOn/Program.cs
+++ HandsOn/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HandsOn
+﻿namespace HandsOn
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //first commit
         }
     }
 }
