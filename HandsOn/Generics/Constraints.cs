feb4e3cec9150701fef56aa5cc2fadf28daafa45
Lakhwinder <lakhwinder_gill@hotmail.com>
2020-06-02 17:02:33 +1000

    Generics, variance and constraints

---
+++ HandsOn/Generics/Constraints.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace HandsOn.Generics
+{
+    public class GenericConstraints
+    {
+        public static void Main(string[] args)
+        {
+            var number = new Nullable<int>(5);
+            Console.WriteLine("Has value? " + number.HasValue);
+            Console.WriteLine("Value : " + number.GetValueOrDefault());
+        }
+        //where T: IComparable      
+        //where T: struct
+        //where T: class
+        //where T: new()
+        //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/constraints-on-type-parameters
+        public class Constraints
+        {
+            public int Max(int a, int b)
+            {
+                return a > b ? a : b;
+            }
+
+            //Generic
+            public T Max<T>(T a, T b) where T : IComparable
+            {
+                return a.CompareTo(b) > 0 ? a : b;
+            }
+           
+        }
+
+        public class Constraints2<T> where T : IComparable, new()
+        {
+
+            //Generic
+            public T Max(T a, T b)
+            {
+                return a.CompareTo(b) > 0 ? a : b;
+            }
+            public void DoSomething(T value)
+            {
+                var obj = new T();
+            }
+        }
+        public class Nullable<T> where T : struct
+        {
+            private object _value;
+            public Nullable()
+            {
+
+            }
+            public Nullable(T value)
+            {
+                _value = value;
+            }
+            public bool HasValue
+            {
+                get { return _value != null; }
+            }
+
+            public T GetValueOrDefault()
+            {
+                if (HasValue)
+                    return (T)_value;
+                return default(T);
+            }
+        }
+
+        public class DiscountCalculator<TProduct> where TProduct : Product
+        {
+            public float CalculateDiscount(TProduct product)
+            {
+                return product.Price;
+            }
+
+        }
+
+        public class Product
+        {
+            public string Title { get; set; }
+            public float Price { get; set; }
+        }
+
+        public class Book : Product
+        {
+            public string Isbn { get; set; }
+        }
+    }
+}
---
+++ HandsOn/Generics/Variance.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace HandsOn
+{
+    public class Variance
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v1 = new Car();
+            //Vehicle v2 = new Car();
+            //Vehicle v3 = new Aircraft();
+            Func<Vehicle> func = GetVehicle; //#1
+            Vehicle v4 = func();// For Func, input param is covariant and output param is conbtra.
+            v4.getVehicle();//Only the Vehicle class properties and methods will be accessiable. Not of Car class.It will peel of anything related to Cars while assigning the object to Vehicle.
+            Action<Car> action = ParkVehicle;
+            action(new Car()); //#2 This is called contravariance, because we assigning more derived class to less derived class.
+            //Action type parameters are IN, means all are contravariant.
+            ICovariant<Vehicle> covariant = (ICovariant<Car>)null;//#1 Covariant - Assigning more derived class to less derived class.
+            IContravariance<Car> contravariance = (IContravariance<Vehicle>)null;
+        }
+
+        //static Vehicle GetVehicle()
+        //{ }
+        static Car GetVehicle()
+        {
+            return new Car();//#1: This method is returning Car but Func<Vehicle> func = GetVehicle; is accepting Vehicle. This is called Co-variance. Assigning the Derived class to Parrent class. Car is covarient with Vehicle.
+        }
+        static void ParkVehicle(Vehicle vehicle)
+        {
+        }
+        public class Vehicle
+        {
+            public void getVehicle() { }
+        }
+
+        class Car : Vehicle
+        {
+            void getsCars()
+            { }
+        }
+
+        class Aircraft : Vehicle
+        {
+
+        }
+
+        interface ICovariant<out T> //Need more understanding
+        {
+            T Name { get; }
+            T Get();
+        }
+        interface IContravariance<in T>//Need more understanding
+        {
+            T Name { set; }
+            void Set(T t);
+        }
+    }
+}
